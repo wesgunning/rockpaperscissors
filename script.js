39d5ff632cca5270e915e2a0d8499904808e5994
Wes Gunning <wesgunning@gmail.com>
2022-10-31 16:23:29 -0400

    Add best of 5 loop

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
+// Best of 5 games wins
+let compScore = 0;
+let userScore = 0;
+for (let i = 0; i < 5; i++) {
+    console.log("Game",i+1,"of 5");
+    game()
+    if (winner == 'player') {
+        userScore++;
+    }
+    else {
+        compScore++;
+    }
+    console.log("Score: \nComputer:",compScore,"\nPlayer:",userScore);
 
+}
+if (compScore > userScore) {
+    console.log("Computer wins the round!");
+}
+else {
+    console.log("Player wins the round!");
+}
 function playRound(playerSelection, computerSelection) {
     console.log("Player chooses: ", playerSelection + "!");
     console.log("Computer chooses: ", computerSelection + "!");
     if (computerSelection == 'Rock') {
         if (playerSelection == 'Paper') {
             console.log("Player wins!");
+            winner = 'player';
         }
         else {
             console.log("Computer wins!");
+            winner = 'computer'
         }
     }
     else if (computerSelection == 'Paper') {
         if (playerSelection == 'Scissors') {
             console.log("Player wins!");
+            winner = 'player';
         }
         else {
             console.log("Computer wins!");
+            winner = 'computer';
         }
     }
     else {
         if (playerSelection == 'Rock') {
             console.log("Player wins!");
+            winner = 'player';
         }
         else {
             console.log("Computer wins!");
+            winner = 'computer';
         }
     }
-
+return;
 }
 function game() {
+let winner;
 const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
@@ -68,6 +95,5 @@ function getPlayerChoice() {
         alert("You did not enter a valid choice. Please try again.")
         getPlayerChoice();
     }
-console.log("User choice:",userChoice);
 return userChoice;
 }
